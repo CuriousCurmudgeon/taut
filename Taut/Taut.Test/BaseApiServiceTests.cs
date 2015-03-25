b2e6c624784faee4d5c35c67e311d436e84b9f11
Brian Meeker <meeker.brian@gmail.com>
2015-03-24 23:10:29 -0400

    Added missing test file.

---
+++ Taut/Taut.Test/BaseApiServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoftwareApproach.TestingExtensions;
+
+namespace Taut.Test
+{
+    [TestClass]
+    public class BaseApiServiceTests
+    {
+        #region BuildRequestUrl
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void WhenMethodIsNull_ThenBuildRequestUrlThrowsException()
+        {
+            // Arrange
+            var apiService = new StubApiService();
+
+            // Act
+            apiService.BuildRequestUrl(null);
+        }
+
+        #endregion
+    }
+
+    internal class StubApiService : BaseApiService {}
+}
