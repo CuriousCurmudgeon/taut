85250f2348d39fcadc4bb9cbac72b8b7cf113997
Brian Meeker <meeker.brian@gmail.com>
2015-03-28 14:08:33 -0400

    Implemented test that loads json data from file.

--- Taut.Test/Channels/ChannelServiceTests.cs
+++ Taut.Test/Channels/ChannelServiceTests.cs
@@ -1,12 +1,10 @@
 ﻿using Flurl.Http.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using SoftwareApproach.TestingExtensions;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Reactive.Threading.Tasks;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Taut.Authorizations;
@@ -18,9 +16,16 @@ namespace Taut.Test.Channels
     public class ChannelServiceTests 
     {
         private const string CHANNEL_INFO_RESPONSE = "{\"ok\":true}";
+        private static ChannelInfoResponse OkChannelInfoResponse;
 
         private HttpTest _httpTest;
         private Mock<IUserCredentialService> _userCredentialService;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            OkChannelInfoResponse = JsonLoader.LoadJson<ChannelInfoResponse>(@"Channels/Data/channel_info.json");
+        }
 
         [TestInitialize]
         public void Initialize()
@@ -56,7 +61,7 @@ namespace Taut.Test.Channels
         {
             // Arrange
             var service = BuildChannelService();
-            _httpTest.RespondWith(CHANNEL_INFO_RESPONSE);
+            _httpTest.RespondWithJson(OkChannelInfoResponse);
             SetAuthorizedUserExpectations();
 
             // Act
@@ -66,6 +71,21 @@ namespace Taut.Test.Channels
             _httpTest.ShouldHaveCalled("*channels.info*channel=123")
                 .WithVerb(HttpMethod.Get)
                 .Times(1);
+        }
+
+        [TestMethod]
+        public async Task WhenResponseIsOk_ThenResponseIncludesChannel()
+        {
+            // Arrange
+            var service = BuildChannelService();
+            _httpTest.RespondWithJson(OkChannelInfoResponse);
+            SetAuthorizedUserExpectations();
+
+            // Act
+            var infoObserver = await service.Info("123").ToTask();
+
+            // Assert
+            infoObserver.Channel.ShouldNotBeNull();
         }
 
         #endregion
---
+++ Taut.Test/JsonLoader.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Taut.Test
+{
+    public class JsonLoader
+    {
+        public static IEnumerable<T> LoadJsonCollection<T>(string filePath)
+        {
+            return ParseJsonArrayTo<T>(ReadFile(filePath));
+        }
+
+        public static T LoadJson<T>(string filePath)
+        {
+            return ParseJsonTo<T>(ReadFile(filePath));
+        }
+
+        private static string ReadFile(string filePath)
+        {
+            using (var reader = new StreamReader(filePath))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        private static IEnumerable<T> ParseJsonArrayTo<T>(string json)
+        {
+            var settings = SerializerSettings;
+            var results = JsonConvert.DeserializeObject<T[]>(json, settings);
+            return results.ToList<T>();
+        }
+
+        private static T ParseJsonTo<T>(string json)
+        {
+            var settings = SerializerSettings;
+            var result = JsonConvert.DeserializeObject<T>(json, settings);
+            return result;
+        }
+
+        private static JsonSerializerSettings _serializerSettings;
+        private static JsonSerializerSettings SerializerSettings
+        {
+            get
+            {
+                return _serializerSettings ?? (_serializerSettings = new JsonSerializerSettings
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                });
+            }
+        }
+    }
+}
