6705a7657f296cb014e941362d386d9292e71f25
Brian Meeker <meeker.brian@gmail.com>
2015-04-26 17:01:51 -0400

    Added IUserServce#List.

---
+++ Taut.Test/Users/UserServiceTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Reactive.Threading.Tasks;
+using System.Text;
+using System.Threading.Tasks;
+using Taut.Users;
+
+namespace Taut.Test.Users
+{
+    [TestClass]
+    public class UserServiceTests : ApiServiceTestBase
+    {
+        private static UserListResponse OkUserListResponse;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            OkUserListResponse = JsonLoader.LoadJson<UserListResponse>(@"IMs/Data/im_list.json");
+        }
+
+        #region List
+
+        [TestMethod]
+        public async Task WhenListIsCalled_ThenUsersListIsIncludedInRequest()
+        {
+            await ShouldHaveCalledTestHelperAsync(OkUserListResponse,
+                async service => await service.List().ToTask(),
+                "*users.list");
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private async Task ShouldHaveCalledTestHelperAsync<T>(T response, Func<IUserService, Task<T>> action,
+            string shouldHaveCalled)
+        {
+            // Arrange
+            var service = BuildUserService();
+            HttpTest.RespondWithJson(response);
+            SetAuthorizedUserExpectations();
+
+            // Act
+            var result = await action.Invoke(service);
+
+            // Assert
+            HttpTest.ShouldHaveCalled(shouldHaveCalled)
+                .WithVerb(HttpMethod.Get)
+                .Times(1);
+        }
+
+        private UserService BuildUserService()
+        {
+            return new UserService(UserCredentialService.Object);
+        }
+
+        #endregion
+    }
+}
---
+++ Taut/Users/UserService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Taut.Authorizations;
+
+namespace Taut.Users
+{
+    public class UserService : BaseAuthenticatedApiService, IUserService
+    {
+        private const string LIST_METHOD = "users.list";
+
+        public UserService(IUserCredentialService userCredentialService)
+            : base(userCredentialService) { }
+
+        public IObservable<UserListResponse> List()
+        {
+            return ObservableApiCall(LIST_METHOD, null,
+                    async (requestUrl, cancellationToken) => await GetResponseAsync<UserListResponse>(requestUrl, cancellationToken));
+        }
+    }
+}
